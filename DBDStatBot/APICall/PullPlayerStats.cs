6cca9381e18ac124901b27840066e4a4d1dc190e
synergize <err0r142@gmail.com>
2019-05-08 01:20:20 -0700

    Add project files.

---
+++ DBDStatBot/APICall/PullPlayerStats.cs
@@ -0,0 +1,39 @@
+﻿using DBDStatBot.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace DBDStatBot.APICall
+{
+    class PullPlayerStats
+    {
+        private static string _downloadNews = null;
+        public static void PlayerStats()
+        {
+            using (var web = new WebClient())
+            {
+                try
+                {
+                    var _url =
+                    string.Format($"http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid={_appID}&key={_steamKey}&steamid={_steamID}");
+                    _downloadNews = web.DownloadString(_url);
+                }
+                catch (WebException msg)
+                {
+                    Console.WriteLine("Profile's Game Stats aren't public. Please pick another Steam ID or change the privacy settings in Steam.");
+                }
+                var result = JsonConvert.DeserializeObject<DaylightStatModel.RootObject>(_downloadNews);
+                Console.WriteLine(result.playerstats.stats[0].name);
+
+                for (int i = 0; i < result.playerstats.stats.Count; ++i)
+                {
+                    var output =
+                        $"{result.playerstats.stats[i].name}: {result.playerstats.stats[i].value}";
+                    Console.WriteLine(output);
+                }
+            }
+        }
+    }
+}
---
+++ DBDStatBot/Models/DaylightStatModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBDStatBot.Models
+{
+    class DaylightStatModel
+    {
+        public class Stat
+        {
+            public string name { get; set; }
+            public double value { get; set; }
+        }
+
+        public class Achievement
+        {
+            public string name { get; set; }
+            public int achieved { get; set; }
+        }
+
+        public class Playerstats
+        {
+            public string steamID { get; set; }
+            public string gameName { get; set; }
+            public List<Stat> stats { get; set; }
+            public List<Achievement> achievements { get; set; }
+        }
+
+        public class RootObject
+        {
+            public Playerstats playerstats { get; set; }
+        }
+    }
+}
